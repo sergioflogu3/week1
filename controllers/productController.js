7a3626be41edd45d07a617bcbbf2f99733d621a2
sergioflogu3 <sergioflogu3@gmail.com>
2022-09-05 21:09:30 -0400

    practica 1

--- controllers/productController.js
+++ controllers/productController.js
@@ -1,51 +1,100 @@
 const fs = require("fs");
 
 exports.getAllProducts = (req, res) => {
-  const products = JSON.parse(
-    fs.readFileSync(`${__dirname}/../data/products.json`)
-  );
+    const products = JSON.parse(
+        fs.readFileSync(`${__dirname}/../data/products.json`)
+    );
 
-  res.status(200).json({
-    status: "success",
-    timeOfRequest: req.requestTime,
-    results: products.length,
-    data: {
-      products,
-    },
-  });
+    res.status(200).json({
+        status: "success",
+        timeOfRequest: req.requestTime,
+        results: products.length,
+        data: {
+            products,
+        },
+    });
 };
 
 exports.addProduct = (req, res) => {
-  const products = JSON.parse(
-    fs.readFileSync(`${__dirname}/../data/products.json`)
-  );
-  products.push(req.body);
-  fs.writeFileSync(`${__dirname}/data/products.json`, JSON.stringify(products));
+    const products = JSON.parse(
+        fs.readFileSync(`${__dirname}/../data/products.json`)
+    );
+    products.push(req.body);
+    fs.writeFileSync(`${__dirname}/data/products.json`, JSON.stringify(products));
 
-  res.status(200).json({
-    status: "success",
-    data: {
-      products,
-    },
-  });
+    res.status(200).json({
+        status: "success",
+        data: {
+            products,
+        },
+    });
 };
 
 exports.getProductById = (req, res) => {
-  const products = JSON.parse(
-    fs.readFileSync(`${__dirname}/../data/products.json`)
-  );
-
-  const foundProduct = products.find((p) => p.id == req.params.id);
-  if (foundProduct) {
-    res.status(200).json({
-      status: "success",
-      data: {
-        product: foundProduct,
-      },
-    });
-  } else {
-    res.status(404).json({
-      status: "not found",
-    });
-  }
+    const products = JSON.parse(
+        fs.readFileSync(`${__dirname}/../data/products.json`)
+    );
+    const foundProduct = products.find((p) => p.id == req.params.id);
+    if (foundProduct) {
+        res.status(200).json({
+            status: "success",
+            data: {
+                product: foundProduct,
+            },
+        });
+    } else {
+        res.status(404).json({
+            status: "not found",
+        });
+    }
 };
+
+exports.updatedProductById = (req, res) => {
+    const products = JSON.parse(
+        fs.readFileSync(`${__dirname}/../data/products.json`)
+    );
+    const product = products.find((p) => p.id == req.params.id);
+    if (product) {
+        product.name = req.body.name;
+        product.price = req.body.price;
+        product.category = req.body.category;
+        products.map(value => {
+            if (value === product){
+                value = product;
+                return value;
+            }
+        });
+        fs.writeFileSync(`${__dirname}/../data/products.json`, JSON.stringify(products));
+        res.status(201).json({
+            status: "success",
+            message: 'Archivo modificado con exito'
+        });
+    } else {
+        res.status(404).json({
+            status: "not found",
+        });
+    }
+}
+
+exports.deleteProductById = (req, res) => {
+    const products = JSON.parse(
+        fs.readFileSync(`${__dirname}/../data/products.json`)
+    );
+    const foundProduct = products.find((p) => p.id == req.params.id);
+    if (foundProduct) {
+        products.forEach((value, key) => {
+            if (value.id === foundProduct.id) products.splice(key, 1);
+        });
+        fs.writeFileSync(`${__dirname}/../data/products.json`, JSON.stringify(products));
+        res.status(204).json({
+            status: "success",
+            message: 'Archivo eliminado con exito'
+        });
+    } else {
+        res.status(404).json({
+            status: "not found",
+        });
+    }
+}
+
+
--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -3,9 +3,11 @@ const productController = require("./../controllers/productController");
 const productRouter = express.Router();
 //routes
 productRouter
-  .route("/")
-  .get(productController.getAllProducts)
-  .post(productController.addProduct);
+    .route("/")
+    .get(productController.getAllProducts)
+    .post(productController.addProduct);
 productRouter.route("/:id").get(productController.getProductById);
+productRouter.route("/:id").put(productController.updatedProductById);
+productRouter.route("/:id").delete(productController.deleteProductById);
 
 module.exports = productRouter;
