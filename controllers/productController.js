9e7539e97fd5f32eab4e490233d3f3c3cdf0df7d
sergioflogu3 <sergioflogu3@gmail.com>
2022-09-06 22:31:24 -0400

    practica

--- controllers/productController.js
+++ controllers/productController.js
@@ -1,9 +1,8 @@
 const fs = require("fs");
+const Product = require('../models/Product');
 
-exports.getAllProducts = (req, res) => {
-    const products = JSON.parse(
-        fs.readFileSync(`${__dirname}/../data/products.json`)
-    );
+exports.getAllProducts = async (req, res) => {
+    const products = await Product.find();
 
     res.status(200).json({
         status: "success",
@@ -15,26 +14,18 @@ exports.getAllProducts = (req, res) => {
     });
 };
 
-exports.addProduct = (req, res) => {
-    const products = JSON.parse(
-        fs.readFileSync(`${__dirname}/../data/products.json`)
-    );
-    products.push(req.body);
-    fs.writeFileSync(`${__dirname}/data/products.json`, JSON.stringify(products));
-
+exports.addProduct = async (req, res) => {
+    const newProduct = await Product.create(req.body);
     res.status(200).json({
         status: "success",
         data: {
-            products,
+            newProduct,
         },
     });
 };
 
-exports.getProductById = (req, res) => {
-    const products = JSON.parse(
-        fs.readFileSync(`${__dirname}/../data/products.json`)
-    );
-    const foundProduct = products.find((p) => p.id == req.params.id);
+exports.getProductById = async (req, res) => {
+    const foundProduct = await Product.findById(req.params.id);
     if (foundProduct) {
         res.status(200).json({
             status: "success",
@@ -49,43 +40,17 @@ exports.getProductById = (req, res) => {
     }
 };
 
-exports.updatedProductById = (req, res) => {
-    const products = JSON.parse(
-        fs.readFileSync(`${__dirname}/../data/products.json`)
-    );
-    const product = products.find((p) => p.id == req.params.id);
-    if (product) {
-        product.name = req.body.name;
-        product.price = req.body.price;
-        product.category = req.body.category;
-        products.map(value => {
-            if (value === product){
-                value = product;
-                return value;
-            }
-        });
-        fs.writeFileSync(`${__dirname}/../data/products.json`, JSON.stringify(products));
-        res.status(201).json({
-            status: "success",
-            message: 'Archivo modificado con exito'
-        });
-    } else {
-        res.status(404).json({
-            status: "not found",
-        });
-    }
+exports.updatedProductById = async (req, res) => {
+    await Product.findOneAndUpdate(req.params.id, req.body);
+    res.status(201).json({
+        status: "success",
+        message: 'Archivo modificado con exito'
+    });
 }
 
-exports.deleteProductById = (req, res) => {
-    const products = JSON.parse(
-        fs.readFileSync(`${__dirname}/../data/products.json`)
-    );
-    const foundProduct = products.find((p) => p.id == req.params.id);
-    if (foundProduct) {
-        products.forEach((value, key) => {
-            if (value.id === foundProduct.id) products.splice(key, 1);
-        });
-        fs.writeFileSync(`${__dirname}/../data/products.json`, JSON.stringify(products));
+exports.deleteProductById = async (req, res) => {
+    const product = await Product.findByIdAndDelete(req.params.id)
+    if (product){
         res.status(204).json({
             status: "success",
             message: 'Archivo eliminado con exito'
--- server.js
+++ server.js
@@ -1,6 +1,18 @@
+const mongoose = require('mongoose');
 const app = require("./app");
 const port = process.env.PORT;
 
+mongoose.connect(process.env.DATABASE, {}).then((con) => {
+    console.log('Connected to mongo');
+    // const p = new Product({
+    //     productName: 'product 1',
+    //     price: 10
+    // });
+    // p.save().then(() =>{
+    //     console.log('saved');
+    // });
+});
+
 app.listen(port, () => {
-  console.log(`App running on port ${port}`);
+    console.log(`App running on port ${port}`);
 });
